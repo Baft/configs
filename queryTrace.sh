91649af322a2f445e76f49b2ddfbe6ef345025ee
baft <casra.ba@gmail.com>
2018-03-10 12:34:34 +0330

    add some configs

---
+++ queryTrace.sh
@@ -0,0 +1,23 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+
+dtrace:::BEGIN
+{
+   printf("%-20s %-20s %-40s %-9s\n", "Who", "Database", "Query", "Time(ms)");
+}
+
+mysql*:::query-start
+{
+   self->query = copyinstr(arg0);
+   self->connid = arg1;
+   self->db    = copyinstr(arg2);
+   self->who   = strjoin(copyinstr(arg3),strjoin("@",copyinstr(arg4)));
+   self->querystart = timestamp;
+}
+
+mysql*:::query-done
+{
+   printf("%-20s %-20s %-40s %-9d\n",self->who,self->db,self->query,
+          (timestamp - self->querystart) / 1000000);
+}
